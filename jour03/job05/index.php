7cd9c8a6fcb745c9967842a88c7e10b0b64af206
Victor Pettinati <victorpettinati@gmail.com>
2023-05-24 13:13:55 +0200

    tableau css + code PhP

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -1,32 +1,67 @@
-<?php
-// La chaîne de caractères
-$str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
-
-// Dictionnaire des voyelles et consonnes
-$dic = array(
-    'consonnes' => 0,
-    'voyelles' => 0
-);
-
-// Parcourir la chaîne et compter les voyelles et consonnes
-for ($i = 0; isset($str[$i]); $i++) {
-    $caractere = $str[$i]; // Récupérer le caractère
-
-    // Vérifier si le caractère est une lettre
-    if (($caractere >= 'a' && $caractere <= 'z') || ($caractere >= 'A' && $caractere <= 'Z')) {
-        // Convertir en minuscule pour comparer
-        if ($caractere == 'A' || $caractere == 'E' || $caractere == 'I' || $caractere == 'O' || $caractere == 'U' || $caractere == 'Y'||
-            $caractere == 'a' || $caractere == 'e' || $caractere == 'i' || $caractere == 'o' || $caractere == 'u' || $caractere == 'y') {
-            $dic['voyelles']++;
-        } else {
-            $dic['consonnes']++;
-        }
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    /* Styles pour le tableau */
+    .table {
+      border-collapse: collapse;
+      width: 40%;
     }
-}
+    .table thead th {
+      background-color: #f2f2f2;
+      border: 1px solid #dddddd;
+      padding: 8px;
+      text-align: left;
+    }
+    .table tbody td {
+      border: 1px solid #dddddd;
+      padding: 8px;
+      text-align: left;
+    }
+  </style>
+</head>
+<body>
+  <?php
+  // La chaîne de caractères
+  $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
 
-// Afficher les résultats dans un tableau HTML
-echo '<table>';
-echo '<thead><tr><th>Voyelles</th><th>Consonnes</th></tr></thead>';
-echo '<tbody><tr><td>' . $dic['voyelles'] . '</td><td>' . $dic['consonnes'] . '</td></tr></tbody>';
-echo '</table>';
-?>
+  // Dictionnaire des voyelles et consonnes
+  $dic = array(
+      'consonnes' => 0,
+      'voyelles' => 0
+  );
+
+  // Parcourir la chaîne et compter les voyelles et consonnes
+  for ($i = 0; isset($str[$i]); $i++) {
+      $caractere = $str[$i]; // Récupérer le caractère
+
+      // Vérifier si le caractère est une lettre
+      if (($caractere >= 'a' && $caractere <= 'z') || ($caractere >= 'A' && $caractere <= 'Z')) {
+          // Convertir en minuscule pour comparer
+          if ($caractere == 'A' || $caractere == 'E' || $caractere == 'I' || $caractere == 'O' || $caractere == 'U' || $caractere == 'Y'||
+              $caractere == 'a' || $caractere == 'e' || $caractere == 'i' || $caractere == 'o' || $caractere == 'u' || $caractere == 'y') {
+              $dic['voyelles']++;
+          } else {
+              $dic['consonnes']++;
+          }
+      }
+  }
+  ?>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Voyelles</th>
+        <th>Consonnes</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?php echo $dic['voyelles']; ?></td>
+        <td><?php echo $dic['consonnes']; ?></td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
+
---
+++ jour03/job06/index.php
@@ -0,0 +1,16 @@
+<?php
+// La chaîne de caractères
+$str = "Les choses que l'on possede finissent par nous posseder.";
+
+// Parcourir la chaîne à l'envers et l'écrire
+for ($i = 0; isset($str[$i]); $i++) {
+    // Rechercher la fin de la chaîne
+}
+
+// $i contient maintenant la longueur de la chaîne
+
+// Parcourir la chaîne à l'envers et l'écrire
+for ($i = $i - 1; $i >= 0; $i--) {
+    echo $str[$i];
+}
+?>
